94d5544e7bd1190e34b5a4981276249621cebeb2
maiza <maiza@maizas-MacBook-Air.local>
2022-10-10 14:49:03 +0100

    README.md added with python variables lesson

---
+++ env_testing.py
@@ -0,0 +1,34 @@
+# python variables
+#used to store data
+#anything in quotation marks is a string
+#any number is an integer
+#yes or no is a boolean
+#if you have a number with a decimal it is a float
+
+#how to find out the type of data stored
+#-type()
+#-print(type(variable name))
+
+first_name = "zakarie" #string
+last_name = "hassan"
+salary = 50 #integer
+travel = 3.5 #float
+
+# print(first_name)
+# print(last_name)
+# print(salary)
+# print(travel)
+
+print("please enter your first name")
+first_name = input()
+print("please enter your last name")
+last_name = input()
+print("hello dear")
+print(first_name)
+print(last_name)
+print("what is your date of birth")
+date_of_birth = input()
+print(date_of_birth)
+print("are you a uk resident")
+uk_resident = input()
+print(uk_resident)
