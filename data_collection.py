19a6606eba806eaef3eebe32cd43602ffaa35bad
maiza <maiza@maizas-MacBook-Air.local>
2022-10-11 15:07:47 +0100

    updated

---
+++ data_collection.py
@@ -0,0 +1,54 @@
+# data collections
+# lists
+# tuples
+# dict
+
+# lists
+# syntax list = ["gcjhgcgh", "fyfy", "jfhfh"]
+
+
+# apply dry
+shopping_list = ["ketchup", "fanta", "eggs", "bread"]
+print(shopping_list)
+print(type(shopping_list))
+shopping_list.append("chicken") # add item to the list
+print(shopping_list)
+print(shopping_list[2])
+shopping_list[2] = "icecream" # relplace eggs with icecream
+print(shopping_list)
+shopping_list.remove("fanta") # how to remove an item from list
+print(shopping_list)
+
+
+
+
+#can list have multiple data types
+multiple_type = [1, 2, 3, "one", "five", "ten"]
+#print(multiple_type)
+#print(multiple_type[2])
+
+
+# tuples
+# immutable - cant be changed - edited -added
+# user_details = DOB - country name - city name -
+# Syntax ("")
+essentials = ("milk", "paracetamol", "drinks")
+print(essentials)
+print(type(essentials))
+# what is the diff between Lists & tuples
+essentials [0] = "coffee"
+print(essentials)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
